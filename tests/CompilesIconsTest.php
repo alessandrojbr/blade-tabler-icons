67bd96de383e467336d6fbf72e6f132347d62a02
Alessandro Ribeiro <alessandrojbr@gmail.com>
2025-04-07 14:13:45 -0300

    refactor: update SVG assertions in CompilesIconsTest for consistency

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -14,9 +14,8 @@ class CompilesIconsTest extends TestCase
     {
         $result = svg('tabler-accessible')->toHtml();
 
-        // Note: the empty class here seems to be a Blade components bug.
         $expected = <<<'SVG'
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-accessible" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-accessible">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
           <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
@@ -24,7 +23,7 @@ class CompilesIconsTest extends TestCase
         </svg>
         SVG;
 
-        $this->assertStringMatchesFormat($result, $expected);
+        $this->assertSame(trim($expected), trim($result));
     }
 
     public function test_can_add_classes_to_icons()
@@ -32,7 +31,7 @@ class CompilesIconsTest extends TestCase
         $result = svg('tabler-accessible', 'w-6 h-6 text-gray-500')->toHtml();
 
         $expected = <<<'SVG'
-        <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-accessible" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-accessible">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
           <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
@@ -40,7 +39,7 @@ class CompilesIconsTest extends TestCase
         </svg>
         SVG;
 
-        $this->assertStringMatchesFormat($expected, $result);
+        $this->assertSame(trim($expected), trim($result));
     }
 
     public function test_can_add_styles_to_icons()
@@ -48,7 +47,7 @@ class CompilesIconsTest extends TestCase
         $result = svg('tabler-accessible', ['style' => 'color: #555'])->toHtml();
 
         $expected = <<<'SVG'
-        <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-accessible" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-accessible">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
           <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
@@ -56,7 +55,7 @@ class CompilesIconsTest extends TestCase
         </svg>
         SVG;
 
-        $this->assertStringMatchesFormat($expected, $result);
+        $this->assertSame(trim($expected), trim($result));
     }
 
     protected function getPackageProviders($app)
