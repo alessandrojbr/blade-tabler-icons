2b7f0ea7f04e5eb077713bae3af49eb0e1fcdacb
Alessandro Ribeiro <alessandrojbr@gmail.com>
2025-04-07 11:47:31 -0300

    Create .php-cs-fixer.dist.php

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->exclude('vendor');
+
+return PhpCsFixer\Config::create()
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => ['default' => 'align_single_space'],
+        'blank_line_before_statement' => ['statements' => ['return']],
+        'cast_spaces' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'phpdoc_no_empty_return' => false,
+        'phpdoc_align' => ['align' => 'left'],
+    ])
+    ->setFinder($finder);
